5c9d1f92eeba8fe9bd4c5d98dbc9d5af2415e1d9
Dain Saint <dain@cipherprime.com>
2026-09-04 20:36:57 -0400

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://cipherprime.com',
+	redirects: {
+		'/games': '/',
+		'/games/pulse/buy': '/games/pulse',
+		'/games/pulse/faq': '/games/pulse',
+		'/games/pulse/behindthemusic': '/games/pulse',
+		'/games/fractal/guide': '/games/fractal',
+		'/games/fractal/buy': '/games/fractal',
+		'/games/fractal/faq': '/games/fractal',
+		'/games/intake/buy': '/games/intake',
+		'/games/splice/buy': '/games/splice',
+		'/games/auditorium/guide': '/games/auditorium',
+		'/games/auditorium/buy': '/games/auditorium',
+	},
+});
